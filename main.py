a6bcf82719dc757503d5e41b54b8d81f174e93a0
KentaroTazawa <tanokurasummer@gmail.com>
2025-07-09 07:38:28 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-
 from flask import Flask, request
+import os
+from analyze import run_analysis  # 分析ロジックを分離
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def handler():
-    print("[INFO] リクエストを受信しました")
-    return "OK", 200
+    return run_analysis()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
