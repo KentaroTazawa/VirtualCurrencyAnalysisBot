cbe5a284cb7f77ff03acbf4e85f149fd3384099a
KentaroTazawa <tanokurasummer@gmail.com>
2025-07-11 12:55:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Hello from Cloud Run!'
+    return "Bot is alive!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
