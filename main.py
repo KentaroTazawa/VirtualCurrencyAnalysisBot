81a724c6db9ee04b31a110ece5be71257f27eec4
KentaroTazawa <tanokurasummer@gmail.com>
2025-07-11 12:46:41 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, request
+from flask import Flask
 import os
-from analyze import run_analysis  # 分析ロジックを分離
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def handler():
-    return run_analysis()
+@app.route('/')
+def home():
+    return 'Hello from Cloud Run!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
